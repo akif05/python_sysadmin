ea0df10b0035f15e638634bbd044afe08fdc7f7c
Akif Yusein <akifyusein@Akifs-Mac.local>
2018-10-28 01:39:40 +0100

    Added sync.sh

---
+++ demo/sync.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Adding all files and pushing ot git repo
+# Need to be provied with comment and directory
+# Example: sync.sh /Users/akifyusein/PycharmProjects "My comment"
+
+if [ $# -ne 2 ]; then
+  echo "Provide comment and directory before pushing to master!"
+  echo $#
+  exit
+fi
+
+if [ ! -d "$1" ]; then
+  echo "Directory $1 not exist."
+  exit
+fi
+
+# Cd to directory which needs to be pushed to git reposytory
+cd $1
+
+git add -A
+git commit -a -m "$2"
+git push -u origin master
