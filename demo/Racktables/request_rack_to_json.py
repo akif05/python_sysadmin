f52343605bb582b1ed80471f4c6cad7f980d13dd
akif05 <36417283+akif05@users.noreply.github.com>
2018-11-02 11:02:59 +0000

    Update request_rack_to_json.py

--- demo/Racktables/request_rack_to_json.py
+++ demo/Racktables/request_rack_to_json.py
@@ -13,7 +13,7 @@ import configparser
 # passwd: my_password
 
 config = configparser.ConfigParser()
-config.read("/Users/akifyusein/.my_a_pass")
+config.read("/Users/adafadsein/.my_a_pass")
 
 password = config.get("myvars", "passwd")
 username = config.get("myvars", "username")
@@ -21,16 +21,7 @@ username = config.get("myvars", "username")
 <<<<<<< HEAD
 r = requests.get('https://racktables-mysite.co..uk/racktables/index.php?page=depot', auth=(username, password))
 
-
-
-#r = requests.get('https://racktables-001.sl5.misp.co.uk/racktables/index.php?page=depot', auth=(''))
-=======
 r = requests.get('https://racktables-mysite.co.uk/racktables/index.php?page=depot', auth=(username, password))
-
-
-
-
->>>>>>> 7f151a5cc833a40a859d1aa49399a6918b775f4d
 
 ## F string examples
 # print(f'Username: {username}')
@@ -39,8 +30,6 @@ r = requests.get('https://racktables-mysite.co.uk/racktables/index.php?page=depo
 ## Thise are working examples!!!
 # with open('new_whit_whit.html', 'w') as f:
 #    print(r.text, file=f)
-
-
 
 # sys.stdout=open('new_wiht_open.html', 'w')
 # print(r.text)
