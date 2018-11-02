11b8455f97b267efa29b524205c938d93b6d4ca1
Akif Yusein <akifyusein@Akifs-Mac.local>
2018-11-02 12:43:21 +0000

    Working write_to file and html request from racktables. Racktables.

--- demo/Racktables/request_rack_to_json.py
+++ demo/Racktables/request_rack_to_json.py
@@ -2,7 +2,13 @@ import requests
 import sys
 import json
 import configparser
+import os.path
+conf_filename = "/Users/akifyusein/.my_a_pass"
 
+print(conf_filename)
+if not os.path.isfile(conf_filename):
+    print(f'File: {conf_filename} does not exist')
+    sys.exit(1)
 # https://stackoverflow.com/questions/924700/best-way-to-retrieve-variable-values-from-a-text-file-python-json
 # read configuration from file
 # file format
@@ -11,24 +17,36 @@ import configparser
 # [myvars]
 # username: my_username
 # passwd: my_password
+if len(sys.argv) != 2:
+    print("Please provide Url to proceede")
+    sys.exit(1)
+
+# Check if url exitst
+
+url = sys.argv[1]
 
 config = configparser.ConfigParser()
-config.read("/Users/adafadsein/.my_a_pass")
+try:
+    config.read(conf_filename)
+except:
+    print(f"Reading file {conf_filename} problem")
+    sys.exit(2)
 
 password = config.get("myvars", "passwd")
 username = config.get("myvars", "username")
 
-<<<<<<< HEAD
-r = requests.get('https://racktables-mysite.co.uk/racktables/index.php?page=depot', auth=(username, password))
+try:
+    r = requests.get(url, auth=(username, password))
+except:
+    print(f"Error accessing {url}")
+    sys.exit(3)
 
+if r.status_code != 200:
+    print(f'Can not get to the URL {url}')
+    sys.exit(4)
 
-=======
-<<<<<<< HEAD
-r = requests.get('https://racktables-mysite.co..uk/racktables/index.php?page=depot', auth=(username, password))
+print(r.text)
 
-r = requests.get('https://racktables-mysite.co.uk/racktables/index.php?page=depot', auth=(username, password))
-
->>>>>>> f52343605bb582b1ed80471f4c6cad7f980d13dd
 ## F string examples
 # print(f'Username: {username}')
 # print(f'passwd: {password}')
@@ -40,3 +58,4 @@ r = requests.get('https://racktables-mysite.co.uk/racktables/index.php?page=depo
 # sys.stdout=open('new_wiht_open.html', 'w')
 # print(r.text)
 # sys.stdout.close()
+
